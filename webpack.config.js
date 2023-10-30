3b173ed0ee76e5d0f765c0edc79136fb2aa558bf
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-10-30 00:22:04 -0600

    feat: configuraciones webpack para build con react

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,27 @@ module.exports = {
     // entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            exclude: /node_modules/,
+            use: [{
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-react',
+                    {
+                        runtime: 'automatic' // react
+                    }
+                  ]
+                ]
+              }
+            }]
+          }
+        ]
     }
 }
